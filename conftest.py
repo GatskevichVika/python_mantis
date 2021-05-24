4de08db59cbc83f1239026ccf0cd96c47dfc7678
GatskevichVika <gatskevich.vika@gmail.com>
2021-05-24 15:35:50 +0300

    Telnet

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import json
 import os.path
-
 from ftputil import FTPHost
 
 from fixture.application import Application
@@ -27,16 +26,16 @@ def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=config['web']['baseUrl'])
+        fixture = Application(browser=browser, config=config)
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_server(request, config):
-    install_server_configuration(config['host']['ftp'], config['host']['username'], config['host']['password'])
-
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
     def fin():
-        restore_server_configuration(config['host']['ftp'], config['host']['username'], config['host']['password'])
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
     request.addfinalizer(fin)
+
 
 def install_server_configuration(host, username, password):
     with FTPHost(host, username, password) as remote:
@@ -44,7 +43,8 @@ def install_server_configuration(host, username, password):
             remote.remove("config_inc.php.bak")
         if remote.path.isfile("config_inc.php"):
             remote.rename("config_inc.php", "config_inc.php.bak")
-        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+        remote.upload("C://Users//Vika//PycharmProjects//python_mantis//resourсes//config_inc.php", "config_inc.php")
+
 
 def restore_server_configuration(host, username, password):
     with FTPHost(host, username, password) as remote:
--- fixture/application.py
+++ fixture/application.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.generator import GeneratorHelper
-
+from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -20,7 +20,9 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.generator = GeneratorHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def open_home_page(self):
         wd = self.wd
